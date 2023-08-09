ba5a0c23c9d916bd24cc807e904470b70b6f6f2e
ElenLaptop <elen98725@gmail.com>
2023-08-09 16:54:33 +0800

    Add testing.py

--- testing.py
+++ testing.py
@@ -30,8 +30,8 @@ G2 = Gp + G1/2
 #        I = 1.58 for 2s to achieve pi-pluse
 expe = bs.ExpScheme(**phy_args)
 expe.sequence = (
-    bs.Section(2, I=1.58), # pi pluse of I
-    bs.Section(3, d=0, G1=G1, G2=G2) # wait for qubit drops back to ground state.
+    bs.Section(s=1, I=1.58), # pi pluse of I
+    bs.Section(s=3, d=0, G1=G1, G2=G2) # wait for qubit drops back to ground state.
 )
 u_sol1, u_sol_section1 = bs.blochsolve(expe, dt)
 bs.blochdrawer.plot(u_sol1, block=True)
