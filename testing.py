7018017b8653b79f3cc50dbcea25bb4822c63ee6
ElenLaptop <elen98725@gmail.com>
2023-08-09 16:56:44 +0800

    Add testing,py

--- testing.py
+++ testing.py
@@ -4,21 +4,21 @@ In this python file, five examples are given, let code speaks for itself.
 """
 import blochsimu as bs
 
+dt = 5e-4 # sampling time for numerical integral
+
+# d  / x \   / -G2  -d   I\ / x \   /   0   \ 
+#----| y | = |  d  -G2  -Q| | y | + |   0   |
+# dt \ z /   \ -I   Q  -G1/ \ z /   \ G1*z0 /
+# default physical arguments:
 phy_args = {
     'u0': (0.0, 0.0, 1.0),
     'z0': 1.0,
     'I' : 0,
     'Q' : 0,
     'd' : 0,
-    'G1': 1 / 85,
-    'G2': 1 / 120,
+    'G1': 0,
+    'G2': 0,
 }
-dt = 5e-4 # sampling time for numerical integral
-
-
-# d  / x \   / -G2  -d   I\ / x \   /   0   \ 
-#----| y | = |  d  -G2  -Q| | y | + |   0   |
-# dt \ z /   \ -I   Q  -G1/ \ z /   \ G1*z0 /
 # G is short for Gamma. 
 # G1: relaxation
 # G2: decoherence
@@ -33,5 +33,5 @@ expe.sequence = (
     bs.Section(s=1, I=1.58), # pi pluse of I
     bs.Section(s=3, d=0, G1=G1, G2=G2) # wait for qubit drops back to ground state.
 )
-u_sol1, u_sol_section1 = bs.blochsolve(expe, dt)
-bs.blochdrawer.plot(u_sol1, block=True)
+u_sol, u_sol_section = bs.blochsolve(expe, dt)
+bs.blochdrawer.plot(u_sol, block=True)
